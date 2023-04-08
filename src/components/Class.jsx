923cbaaa99a0c540415bfd9d2d8653122aedc52b
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-04-08 15:12:12 +0600

    route declare done

---
+++ src/components/Class.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Class = () => {
+    return (
+        <div>
+            Class page
+        </div>
+    );
+};
+
+export default Class;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,43 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Main from './layouts/Main';
+import Home from './components/Home';
+import Activity from './components/Activity';
+import Departments from './components/Departments';
+import Class from './components/Class';
+import Contract from './components/Contract';
+import Registration from './components/Registration';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <Main></Main>
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'activity',
+        element:<Activity></Activity>
+      },
+      {
+        path:'departments',
+        element:<Departments></Departments>
+      },
+      {
+        path:'class',
+        element:<Class></Class>
+      },
+      {
+        path:'contract',
+        element:<Contract></Contract>
+      },
+      {
+        path:'registration',
+        element:<Registration></Registration>
+      }
+    ]
   }
 ])
 
