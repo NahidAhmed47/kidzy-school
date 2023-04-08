b80bd8f17e35dc69fc6320cd71962887b668934b
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-04-08 21:48:26 +0600

    home page designed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import onlineEducationAni from '../assets/online-learning.json';
+import {PlayIcon} from '@heroicons/react/24/solid'
 const Home = () => {
     return (
-        <div>
-            Home page
+        <div className='grid md:grid-cols-2 w-full'>
+            <div className='px-5 flex flex-col justify-center'>
+                <h1 className='text-lg md:text-5xl font-extrabold secondary-text-color tracking-wide max-w-'>Learn and playing, It's time to achieve yours dreams!</h1>
+                <p className='text-base font-medium text-slate-500 my-2 md:my-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis autem atque. Quia libero labore aliquam velit ad nesciunt aliquid ipsa, ipsum ipsam perspiciatis enim nobis culpa a fuga et provident, porro, commodi eligendi! Voluptatum pariatur voluptates modi reprehenderit quisquam!</p>
+                <div className='w-1/3 flex fle'>
+                    <Link to='/class'>
+                        <button className='btn'>Start Class</button>
+                    </Link>
+                    <Link to='/class'>
+                        <button className='p-2 bg-orange-300 rounded-full'><PlayIcon className='w-6 h-6 primary-text-color'></PlayIcon></button>
+                    </Link>
+                </div>
+            </div>
+            <div>
+                <Lottie animationData={onlineEducationAni} loop={true}></Lottie>
+            </div>
         </div>
     );
 };
